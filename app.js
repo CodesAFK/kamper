cab828f798cd46bc89309d08d83cf7c961539575
Apex-Code <upstaterounder@gmail.com>
2017-12-21 23:20:45 -0600

    added functionality to new.ejs, update campgrounds.ejs with link to create new campground

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 app = express();
+var bodyParser = require('body-parser');
 const server = 1337;
+var campgrounds = [
+    {name:"Beaver Bend", image:"https://static.pexels.com/photos/558454/pexels-photo-558454.jpeg"},
+    {name:"Foss Creek", image:"https://static.pexels.com/photos/216675/pexels-photo-216675.jpeg"},
+    {name:"Arch Linux Pass", image:"https://static.pexels.com/photos/5922/wood-holiday-vacation-vintage.jpg"}
+];
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 // =============================
@@ -13,15 +20,25 @@ app.get('/', function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name:"Beaver Bend", image:"https://static.pexels.com/photos/558454/pexels-photo-558454.jpeg"},
-        {name:"Foss Creek", image:"https://static.pexels.com/photos/216675/pexels-photo-216675.jpeg"},
-        {name:"Arch Linux Pass", image:"https://static.pexels.com/photos/5922/wood-holiday-vacation-vintage.jpg"}
-    ];
 
     res.render('campgrounds', {campgrounds:campgrounds});
 });
 
+app.get('/campgrounds/new', function(req, res){
+    res.render('new');
+});
+
+// =============================
+// POST Routes                ==
+// =============================
+
+app.post('/campgrounds', function(req, res){
+  var name = req.body.campgroundName;
+  var image = req.body.campgroundImg;
+  var newCampground = {name:name, image:image};
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+});
 
 
 // =============================
