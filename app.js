087f4a07d10177147d91b746364b8b0979f6fb9e
Apex-Code <upstaterounder@gmail.com>
2018-01-08 20:59:23 -0600

    added login get and post route with user authentication via passport

--- app.js
+++ app.js
@@ -158,6 +158,22 @@ app.post('/register', function(req, res){
             });
     });
 });
+
+//  SHOW LOGIN FORM
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local',
+    {
+
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+
+    }), function (req, res){
+
+});
 // =============================
 // Start Server               ==
 // =============================
