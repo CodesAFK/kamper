d26ca0f66ec11c8d894cadd63e349395cb6415a9
Apex-Code <upstaterounder@gmail.com>
2018-01-10 00:45:37 -0600

    update routing for campgrounds to include edit and update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -41,6 +41,32 @@ router.get('/:id', function (req, res) {
 });
 
 // =============================
+// EDIT CAMPGROUND            ==
+// =============================
+
+router.get('/:id/edit', function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+          console.log(err);
+          res.redirect("/campgrounds");
+      }else{
+          res.render('campgrounds/edit', {campground:foundCampground});
+      }
+  });
+
+});
+
+router.put('/:id', function(req, res){
+   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           console.log(err);
+           res.redirect('/campgrounds');
+       } else {
+           res.redirect('/campgrounds/' + req.params.id);
+       }
+   })
+});
+// =============================
 // POST Routes                ==
 // =============================
 
