e85568079906280b96fac17ee509032dd43d9fb2
Apex-Code <upstaterounder@gmail.com>
2018-01-15 22:31:09 -0600

    update app.js to fix issue with persistence

--- app.js
+++ app.js
@@ -1,55 +1,47 @@
-var express           = require('express'),
-    app               = express(),
-    mongoose          = require('mongoose'),
-    bodyParser        = require('body-parser'),
-    Campground        = require('./models/campground'),
-    passport          = require('passport'),
-    LocalStrategy     = require('passport-local'),
-    Comment           = require("./models/comment"),
-    User              = require("./models/user"),
-    methodOverride    = require('method-override');
-    seedDB            = require("./seeds");
-
-var commentsRoute     = require("./routes/comments");
-var campgroundsRoute  = require("./routes/campgrounds");
-var authRoute         = require("./routes/index");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
 
 const server = 1337;
 
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+
 mongoose.connect("mongodb://localhost/kamper");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+// seedDB(); //seed the database
 
-
-// =============================
-// == PASSPORT CONFIGURATION  ==
-// =============================
-
-app.use(require('express-session')({
-    secret:"This isn't the deployment secret.",
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-// Share current user across pages
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     next();
 });
 
-app.use("/campgrounds/:id/comments", commentsRoute);
-app.use("/campgrounds", campgroundsRoute);
-app.use(authRoute);
-
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 // =============================
 // Start Server               ==
